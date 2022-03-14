0237e91c9c826ae65b09f75ab6e4d6d10b2d0f31
Even <matencio.even@gmail.com>
2022-03-14 16:19:04 +0100

    Commented code.

--- points.py
+++ points.py
@@ -10,6 +10,10 @@ class Point():
         self._x = x
         self._y = y
 
+# -------------------------------------------------------------------------------------------------------------------
+# READERS AND WRITERS
+# -------------------------------------------------------------------------------------------------------------------
+
     @property
     def x(self):
         return self._x
@@ -26,6 +30,26 @@ class Point():
     def y(self, ny):
         self._y = int(ny)
 
+
+# -------------------------------------------------------------------------------------------------------------------
+# USEFUL METHODS
+# -------------------------------------------------------------------------------------------------------------------
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, point2):
+        return self.x == point2.x and self.y == point2.y
+
+    def __sub__(self, point2):
+        """
+        Perform the subtraction of self minus point2.
+        """
+        return Point(self.x - point2.x, self.y - point2.y)
+
+    def norm2(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
     @staticmethod
     def from_random(x_max, y_max):
         """
@@ -36,17 +60,6 @@ class Point():
         assert x_max >0 and y_max>0,  "negative entries"
         return Point(rd.randint(0, x_max-1), rd.randint(0, y_max-1))
 
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    def __eq__(self, point2):
-        return self.x == point2.x and self.y == point2.y
-
-    def __sub__(self, point2):
-        return Point(self.x - point2.x, self.y - point2.y)
-
-    def norm2(self):
-        return math.sqrt(self.x**2 + self.y**2)
 
 
 if __name__ == "__main__":
