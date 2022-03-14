736770cb9b625576753e3f127f5c30ed493986b5
Even <matencio.even@gmail.com>
2022-03-14 16:19:12 +0100

    Commented code.

--- scooter.py
+++ scooter.py
@@ -10,9 +10,29 @@ sys.path.append(os.path.abspath("./"))
 from points import *
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as plt_colors
 
+# -------------------------------------------------------------------------------------------------------------------
+# GLOBAL VARIABLES
+# -------------------------------------------------------------------------------------------------------------------
+
+
+SPACE_STEP = 50
+"The distance separating two points of the map such that norm2(point1-point2) = 1 [meter]"
+MAP_SIZE = 200
+"The size of the map on which the scooters move [SPACE_STEP]"
 
 class Scooter():
+
+    # -------------------------------------------------------------------------------------------------------------------
+    # CLASS VARIABLES AND INITIALIZER
+    # -------------------------------------------------------------------------------------------------------------------
+
+    AVERAGE_DISTANCE = 20000
+    "Average distance ran by a scooter on a trip [meter]"
+    AVERAGE_MASS = 70
+    "Averge mass of the scooters'users [kilogram]"
+
 
     def __init__(self, coord=Point(0, 0), soc=100):
         """
@@ -23,7 +43,11 @@ class Scooter():
         self._coord = coord
         self._moving = False
         self._destination = self._coord
-        self._mass_user = rd.randint(50,120)
+        self._mass_user = rd.randint(self.AVERAGE_MASS-20, self.AVERAGE_MASS+50)
+
+    # -------------------------------------------------------------------------------------------------------------------
+    # READERS AND WRITERS
+    # -------------------------------------------------------------------------------------------------------------------
 
     @property
     def coord(self):
@@ -63,20 +87,28 @@ class Scooter():
     def destination(self, destin):
         self._destination = destin
 
+    # -------------------------------------------------------------------------------------------------------------------
+    # USEFUL METHODS
+    # -------------------------------------------------------------------------------------------------------------------
+
     def move(self):
-        assert self.moving == True, "not moving"
+        """
+        This method performs one step of simulation.
+        A scooter first moves on x_axis and after on y_axis.
+        """
+        assert self.moving, "not moving"
         if self.coord.x != self.destination.x:
             self.coord.x = (self.coord.x + (self.destination.x - self.coord.x) / abs(self.destination.x - self.coord.x))
-            self.soc = self.soc-100*(50/20000)*(1+(self.mass_user-70)/70)
+            self.soc = self.soc-100*(SPACE_STEP/self.AVERAGE_DISTANCE)*(1+(self.mass_user-self.AVERAGE_MASS)/self.AVERAGE_MASS)
         elif self.coord.y != self.destination.y:
             self.coord.y = (self.coord.y + (self.destination.y - self.coord.y) / abs(self.destination.y - self.coord.y))
-            self.soc = self.soc - 100 * (50 / 20000) * (1 + (self.mass_user - 70) / 70)
+            self.soc = self.soc-100*(SPACE_STEP/self.AVERAGE_DISTANCE)*(1+(self.mass_user-self.AVERAGE_MASS)/self.AVERAGE_MASS)
         if self.coord == self.destination:
             self.moving = False
 
-    def init_new_trip(self,t):
-        if(self.moving==False):
-            
+    # def init_new_trip(self,t):
+    #     if(self.moving==False):
+
 
 
 
@@ -93,23 +125,11 @@ def simulation():
     return x, y
 
 
-
 if __name__ == "__main__":
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection='2d')
 
     x, y = simulation()
-
-    # # create the first plot
-    # point, = ax.plot([x[0]], [y[0]], 'o')
-    # line, = ax.plot(x, y, z, label='parametric curve')
-    # ax.legend()
-    # ax.set_xlim([-1, 10])
-    # ax.set_ylim([-1, 10])
-    #
-    # ani = animation.FuncAnimation(fig, update_point, 15, fargs=(x, y, point))
-
     fig, ax = plt.subplots()
+
 
     for t in range(50):
         if t == 0:
