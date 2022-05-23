c9a96169950cf7ba299fcb7b47cf166329a1817c
Raphael Taisant <raphael.taisant@gmail.com>
2022-05-23 16:13:35 +0200

    multiprocessing

--- main.py
+++ main.py
@@ -9,14 +9,32 @@ from scooter import *
 from strategy import *
 import json
 
+from multiprocessing import Pool
+
 # -------------------------------------------------------------------------------------------------------------------
 # GLOBAL VARIABLES
 # -------------------------------------------------------------------------------------------------------------------
 
-TIME_RANGE = 5*DAY_LENGTH
+TIME_RANGE = 5 * DAY_LENGTH
 '''number of steps of the simulation'''
 SIZE_OF_FLEET = 100
 '''number of scooter in our simulation'''
+
+
+def exec_simul(input):
+    strategy = FirstChargingStrategy(TIME_RANGE, SIZE_OF_FLEET, render=False, verbose=False)
+    strategy.set_parameters(charging_slot=input[0], discharged_proportion=input[1],
+                            discharge_threshold=input[2],
+                            pick_up_threshold=input[3], charging_level=input[4])
+    strategy.launch()
+    # Storing the data
+    param = {"charging_slot": input[0], "discharged_proportion": input[1],
+             "discharge_threshold": input[2], "pick_up_threshold": input[3],
+             "charging_level": input[4]}
+    cost = {"transport": strategy.transporting_cost, "distribution": strategy.repartition_cost}
+    total_cost = strategy.transporting_cost + strategy.repartition_cost
+    simulation = {"id": input[5], "param": param, "cost": cost, "total_cost": total_cost}
+    return simulation
 
 
 # -------------------------------------------------------------------------------------------------------------------
@@ -37,26 +55,33 @@ if __name__ == "__main__":
 
     results_list = []
     id = 0
-    for discharged_thresh in np.linspace(10, 30, 1):
-        for discharged_prop in np.linspace(0.2, 0.7, 1):
-            for pick_up_threhs in np.linspace(15, 25, 1):
-                for charging_lev in np.linspace(75, 85, 1):
-                    for charg_slot in [DAY_LENGTH//3]:#, DAY_LENGTH//2, DAY_LENGTH, 2*DAY_LENGTH]:
-                        print(f"\n Simulation n°{id}")
-                        # Running the simulation
-                        strategy = FirstChargingStrategy(TIME_RANGE, SIZE_OF_FLEET, render=False, verbose=True)
-                        strategy.set_parameters(charging_slot=charg_slot, discharged_proportion=discharged_prop,
-                                                discharge_threshold=discharged_thresh,
-                                                pick_up_threshold=pick_up_threhs, charging_level=charging_lev)
-                        strategy.launch()
-                        # Storing the data
-                        param = {"charging_slot" : charg_slot, "discharged_proportion" : discharged_prop,
-                                 "discharge_threshold" : discharged_thresh, "pick_up_thershold" : pick_up_threhs,
-                                 "charging_level" : charging_lev}
-                        cost = {"transport" : strategy.transporting_cost, "distribution" : strategy.repartition_cost}
-                        simulation = {"id" : id,  "param" : param, "cost" : cost}
-                        id+=1
-                        results_list.append(simulation)
+    charging_lev = 80
+    inputs_list = []
+    for discharged_thresh in np.linspace(10, 18, 3):  # 3
+        for discharged_prop in np.linspace(0.2, 0.8, 4):  # 4
+            for pick_up_threhs in np.linspace(15, 24, 4):  # 4
+                for charg_slot in [DAY_LENGTH // 3, DAY_LENGTH // 2, DAY_LENGTH]:
+                    # print(f"\n Simulation n°{id}")
+                    inputs_list.append([charg_slot, discharged_prop, discharged_thresh, pick_up_threhs, charging_lev, id])
+                    id+=1
+
+                    # # Running the simulation
+                    # strategy = FirstChargingStrategy(TIME_RANGE, SIZE_OF_FLEET, render=False, verbose=False)
+                    # strategy.set_parameters(charging_slot=charg_slot, discharged_proportion=discharged_prop,
+                    #                         discharge_threshold=discharged_thresh,
+                    #                         pick_up_threshold=pick_up_threhs, charging_level=charging_lev)
+                    # strategy.launch()
+                    # # Storing the data
+                    # param = {"charging_slot" : charg_slot, "discharged_proportion" : discharged_prop,
+                    #          "discharge_threshold" : discharged_thresh, "pick_up_threshold" : pick_up_threhs,
+                    #          "charging_level" : charging_lev}
+                    # cost = {"transport" : strategy.transporting_cost, "distribution" : strategy.repartition_cost}
+                    # total_cost = strategy.transporting_cost + strategy.repartition_cost
+                    # simulation = {"id" : id,  "param" : param, "cost" : cost, "total_cost" : total_cost}
+                    # id+=1
+                    # results_list.append(simulation)
+    with Pool(4) as p:
+        results_list = p.map(exec_simul, inputs_list)
 
     print("\n ________________________________ \n")
     print(f"Nombre simul = {len(results_list)}")
@@ -66,17 +91,7 @@ if __name__ == "__main__":
     with open('simul1_output.json', 'w') as outfile:
         json.dump(results_list, outfile)
 
-
-
-
-
-
-
-
-
-
     # strategy.init_plot()
     # strategy.points.append(strategy.ax.plot(INTEREST_POINT_2.x, INTEREST_POINT_2.y, marker='s', linestyle='None', markersize=10, color='b')[0])
     # strategy.points.append(strategy.ax.plot(INTEREST_POINT_1.x, INTEREST_POINT_1.y, marker='s', linestyle='None', markersize=10, color='b')[0])
     # plt.show()
-
